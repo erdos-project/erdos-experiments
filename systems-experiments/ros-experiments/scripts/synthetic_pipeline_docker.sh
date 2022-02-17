ccb4cc1b676902c5cdf0bf66c81cc2e22041e6fc
Peter Schafhalter <pschafhalter@berkeley.edu>
2022-02-16 16:31:40 -0800

    [ROS] Add synthetic pipeline experiment

---
+++ systems-experiments/ros-experiments/scripts/synthetic_pipeline_docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker run -it -h `hostname` --mount type=bind,src="$(pwd)/../",dst=/experiments \
+    ros:noetic /bin/bash -c "cd /experiments/ros-experiments;
+    . scripts/setup_container.sh;
+    . scripts/synthetic_pipeline.sh"
