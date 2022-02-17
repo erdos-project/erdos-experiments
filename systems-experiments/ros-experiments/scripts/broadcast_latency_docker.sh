4fab87bee58548a111caeeb527b9e195e4b94245
Peter Schafhalter <pschafhalter@berkeley.edu>
2022-02-16 16:30:51 -0800

    Use ROS noetic

--- systems-experiments/ros-experiments/scripts/broadcast_latency_docker.sh
+++ systems-experiments/ros-experiments/scripts/broadcast_latency_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker run -it -h `hostname` --mount type=bind,src="$(pwd)/../",dst=/experiments \
-    ros:melodic /bin/bash -c "cd /experiments/ros-experiments;
+    ros:noetic /bin/bash -c "cd /experiments/ros-experiments;
     . scripts/setup_container.sh;
     . scripts/broadcast_latency.sh"
