249018265bdd770ccb6c9864e59033a73713fd32
Peter Schafhalter <pschafhalter@berkeley.edu>
2022-03-11 11:44:29 -0800

    [ROS2] Update scripts

--- systems-experiments/ros2-experiments/scripts/run_all_docker.sh
+++ systems-experiments/ros2-experiments/scripts/run_all_docker.sh
@@ -3,4 +3,6 @@ docker run -it -h `hostname` --mount type=bind,src="$(pwd)/../",dst=/experiments
     ros:galactic /bin/bash -c "cd /experiments/ros2-experiments;
     . scripts/setup_container.sh;
     . scripts/broadcast_latency_intra_process.sh;
-    . scripts/msg_size_latency_intra_process.sh"
+    . scripts/broadcast_latency_inter_process.sh;
+    . scripts/msg_size_latency_intra_process.sh;
+    . scripts/msg_size_latency_inter_process.sh"
